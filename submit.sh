d04229756971f3f3313f6f9f508705382376c756
will <githubutilities163@163.com>
2014-11-13 20:44:18 +0800

    updating submit.sh bootstrap

--- submit.sh
+++ submit.sh
@@ -1,3 +1,7 @@
-git add .
-git commit -m "$1"
-git push -u origin master
+if [ $# -eq 0 ] # '$#' return the number of argument
+	then echo "Usage: submit.sh commit_message"
+else
+	git add .
+	git commit -m "$1"
+	git push -u origin master
+fi
